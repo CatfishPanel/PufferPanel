e5522d9236bac0963fac2ce164b333e777f8d5fb
Lord_Ralex <lordralex@gmail.com>
2015-08-11 04:02:36 -0500

    Add dynamic language selection - No idea if it works though on the view, but the browser + meteor are aware of it - Clean routes for future "api"/"module" system

--- client/home/index.js
+++ client/home/index.js
@@ -5,3 +5,11 @@ Template.homeIndex.helpers({
       return Servers.find();
     }
 });
+
+Template.navbar.events({
+   'click .language-button': function(e) {
+       e.preventDefault();
+       //get language from selected html and set into session
+       Meteor.call('changeLanguage', $(e.currentTarget).attr('language'), function(err, response) {});
+   }
+});
