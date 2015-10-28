d82dcca9490cf85bf11635244208fa748cb8928f
Lord_Ralex <lordralex@gmail.com>
2015-10-28 16:43:06 -0500

    Migrate server view to base, add selection support - Will 403 if the server does not belong or exist to the user - Fix 403 page

--- modules/minecraft/main.js
+++ modules/minecraft/main.js
@@ -2,13 +2,6 @@ var Rfr = require('rfr');
 var ServerApi = Rfr('lib/api/servers.js');
 
 ServerApi.registerType('minecraft', {
-    '': {
-        method: 'GET',
-        handler: function (request, response, server) {
-
-            response.view('minecraft/index.html');
-        }
-    },
     'index': {
         method: 'GET',
         handler: function (request, response, server) {
